bd4caa8faffecb69f7edc95b50c371c15dba7acb
Shre1100 <k.shreya7519@gmail.com>
2025-03-16 00:44:14 +0530

    Initialize Doctors page

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -12,25 +12,25 @@ const Navbar = () =>{
 
     return(
         <div className=" flex bg-white items-center justify-between mt-2 rounded-lg text-sm py-4 pr-7 mb-5 border-b border-b-gray-400 h-[60px] shadow-lg shadow-slate-900/20 shadow-b-3 shadow-r-[3px] -shadow-spread-2">
-          <img className="h-[60px] cursor-pointer w-44" src={assets.logo} alt="mediConnect logo" /> 
+          <NavLink to='/'><img className="h-[60px] cursor-pointer w-44" src={assets.logo} alt="mediConnect logo" /> </NavLink>
           <div className="flex gap-14 items-center">
           <ul className="hidden md:flex justify-evenly gap-14 items-start font-semibold">
             {/* navigation links in the navbar  */}
             <NavLink to='/'>
-                <li className="py-1 text-[1.04rem]">Home</li>
-                <hr className="border-none outline-none h-0.5 bg-primary m-auto hidden"/>
+                <li className="py-1 text-[1.04rem] rounded-md hover:shadow-xl hover:scale-100 transition-all hover:bg-slate-200 px-2">Home</li>
+                <hr className="border-none outline-none h-0.5 bg-primary mt-1 m-auto hidden"/>
             </NavLink>
             <NavLink to='/doctors'>
-                <li className="py-1 text-[1.04rem]">Doctors</li>
-                <hr className="border-none outline-none h-0.5 bg-primary m-auto hidden"/>
+                <li className="py-1 text-[1.04rem] rounded-md hover:shadow-xl hover:scale-100 transition-all hover:bg-slate-200 px-2">Doctors</li>
+                <hr className="border-none outline-none h-0.5 bg-primary mt-1 m-auto hidden"/>
             </NavLink>
             <NavLink to='/about'>
-                <li className="py-1 text-[1.04rem]">About Us</li>
-                <hr className="border-none outline-none h-0.5 bg-primary m-auto hidden"/>
+                <li className="py-1 text-[1.04rem] rounded-md hover:shadow-xl hover:scale-100 transition-all hover:bg-slate-200 px-2">About Us</li>
+                <hr className="border-none outline-none h-0.5 bg-primary mt-1 m-auto hidden"/>
             </NavLink>
             <NavLink to='/contact'>
-                <li className="py-1 text-[1.04rem]">Contact</li>
-                <hr className="border-none outline-none h-0.5 bg-primary m-auto hidden"/>
+                <li className="py-1 text-[1.04rem] rounded-md hover:shadow-xl hover:scale-100 transition-all hover:bg-slate-200 px-2">Contact</li>
+                <hr className="border-none outline-none h-0.5 bg-primary mt-1 m-auto hidden"/>
             </NavLink>
           </ul>
           <div className="flex items-center gap-4 mr-8">
--- frontend/src/pages/Doctors.jsx
+++ frontend/src/pages/Doctors.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
+import {useParams} from "react-router-dom";
 
 const Doctors = () => {
+    const {speciality} = useParams();
+
+    // const {doctors} = useContext(AppContext);
     return(
         <div>
-            hello i am at doctors.
+            <p>Here is the list of all trusted Doctors.<br/>You can simply search by applying filters.</p>
         </div>
     )
 }
