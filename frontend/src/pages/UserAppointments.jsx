27a71758b44fc758494039229a9b5dc190957539
Shre1100 <k.shreya7519@gmail.com>
2025-04-23 13:26:15 +0530

    Start user appointment page

--- frontend/src/pages/UserAppointments.jsx
+++ frontend/src/pages/UserAppointments.jsx
@@ -2,7 +2,22 @@ import React from "react";
 
 const UserAppointments = () =>{
     return(
-        <div>
+        <div className="mx-20 bg-slate-200 flex flex-col items-center">
+            <div className="flex justify-center items-center my-5">
+                <h2 className="font-semibold text-3xl">My Appointments</h2>
+            </div>
+            <div className="flex bg-white w-full my-5 items-center justify-center gap-10">
+                <div>
+                    <p className="text-xl">Upcoming</p>
+                </div>
+                <p>|</p>
+                <div>
+                    <p className="text-xl">Completed</p>
+                </div>
+            </div>
+            <div>
+
+            </div>
 
         </div>
     )
