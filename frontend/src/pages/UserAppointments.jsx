814c0202306c5e655ad9f467b7dc62ea0d277a46
Shre1100 <k.shreya7519@gmail.com>
2025-04-26 23:44:14 +0530

    complete ui for appointment page for user

--- frontend/src/pages/UserAppointments.jsx
+++ frontend/src/pages/UserAppointments.jsx
@@ -1,11 +1,15 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
+import {AppContext} from '../context/AppContext';
 
 const UserAppointments = () =>{
 
     const [state, setState] = useState('Upcoming');
 
+    const {doctors} = useContext(AppContext);
+
     return(
-        <div className="h-[100vh] md:mx-3 lg:mx-20 bg-white flex flex-col items-center">
+        // h-[100vh]
+        <div className=" md:mx-3 lg:mx-20 bg-white flex flex-col items-center">
             <div className="flex justify-center items-center my-3 md:my-5">
                 <h2 className="font-semibold text-xl md:text-2xl lg:text-3xl">My Appointments</h2>
             </div>
@@ -18,8 +22,30 @@ const UserAppointments = () =>{
                     <p className={`text-base transition-all duration-300 text-gray-700 font-semibold md:text-xl ${state === 'Completed'? "bg-white px-4 py-1 rounded-lg shadow-md":""}`}>Completed</p>
                 </div>
             </div>
-            <div>
 
+            {/* displaying appointments  */}
+            <div className="flex flex-col gap-10 w-full">
+                {
+                doctors.slice(0,2).map((item,index)=>(
+                    <div key={index} className="flex w-full items-center justify-between border-b-2">
+                        <div className="bg-slate-200 rounded-full my-2 mx-3 border border-solid border-gray-700">
+                            <img className="rounded-full hidden sm:block sm:h-[100px] lg:h-[130px] lg:w-[130px]" src={item.image} alt="Doctor image" />
+                        </div>
+                        <div className="flex flex-col flex-1 justify-start">
+                            <p className="font-semibold text-lg">{item.name}</p>
+                            <p className="text-sm text-gray-600">{item.speciality}</p>
+                            <p className="font-semibold">Address :</p>
+                            <p>{item.address.line1} {item.address.line2}</p>
+                            <p><span className="font-semibold">Date & Time: </span>12 june 2024</p>
+                        </div>
+                        <div></div>
+
+                        <div className="flex flex-col lg:flex-row gap-5 mx-6">
+                            <button className="hover:scale-105 transition-all duration-300 font-semibold text-sm lg:text-base px-5 py-2 bg-slate-300 rounded-2xl">Pay Online</button>
+                            <button className="hover:scale-105 transition-all duration-300 font-semibold text-sm lg:text-base px-5 py-2 bg-red-600 rounded-2xl text-white">Cancel</button>
+                        </div>
+                    </div>
+                ))}
             </div>
 
         </div>
