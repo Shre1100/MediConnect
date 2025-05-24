2f482947a42e69ba07ec1cc0ea0e8bb577c90741
Shre1100 <k.shreya7519@gmail.com>
2025-05-24 17:40:16 +0530

    Update vercel.json

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,8 +1,19 @@
 import mongoose from "mongoose";
 
-const connectDB = async () => {
-    mongoose.connection.on('connected', ()=> console.log("DB connection established"));
-    await mongoose.connect(`${process.env.MONGODB_URI}/mediConnect`);
+const MONGODB_URI = process.env.MONGODB_URI + "/mediConnect";
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
 }
+
+const connectDB = async () => {
+  if (cached.conn) return cached.conn;
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI).then((mongoose) => mongoose);
+  }
+  cached.conn = await cached.promise;
+  return cached.conn;
+};
 
 export default connectDB;
